db6058ff213e642d636dd1e3166960f9173a38d8
Alrik Hausdorf <admin@morulia.de>
2011-11-18 21:42:24 +0100

    Add presenter from existing source

--- main/main.js
+++ main/main.js
@@ -12,14 +12,18 @@ var main={
 	*/
 	init:function(){
 		$('#singleDeck').click(function(){main.singleDeck();});
-		
+		$('#OpenPresenter').click(function(){main.OpenPresenter();});
 	},
-	singleDeck:function(){
+	OpenDeck:function(){
 	this.watch = window.open('./watch/index.html', "watch", "status=0,scrollbars=0,fullscreen=1");
 	this.watch.focus();	
 	
+	},
+	OpenPresenter:function(){
+	this.presenter = window.open('./presenter/index.html', "presenter", "status=0,scrollbars=0,fullscreen=1");
+	this.presenter.focus();	
+	
 	}
-
 
 
 }
