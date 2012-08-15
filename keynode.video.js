082bfa481b7f38bc59bd6b825e3c32afc8f06585
Andy Wermke <a.wermke@gmail.com>
2012-08-15 23:43:02 +0200

    Development progress.

--- keynode.video.js
+++ keynode.video.js
@@ -1,16 +1,34 @@
 
 var VideoAddon = {
+    socketEventsBound : false,
     
+    $embedContainer : null,
+    embedHTML : null
 };
 
 
 $(document).bind('watcher.init', function() {
-    
+    $('body').append(
+        VideoAddon.$embedContainer = $('<div id="video-container"></div>')
+    );
 });
 
 $(document).bind('watcher.bindSocketEvents', function() {
-    var socket = mysocket.getSocket();
+    if(VideoAddon.socketEventsBound)
+        return;
     
+    var socket = mysocket.getSocket();
+    if(!socket)     // is triggered multiple times by the presenter
+        return;
+    
+    socket.on('videoEmbedTag', function(html) {
+        if(html == VideoAddon.embedHTML)
+            return;
+        
+        VideoAddon.embedHTML = html;
+        VideoAddon.$embedContainer.html(html);
+    });
+    VideoAddon.socketEventsBound = true;
 });
 
 $(document).bind('watcher.setOnline', function() {
